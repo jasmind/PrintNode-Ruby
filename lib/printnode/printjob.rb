04cc2f6df2931f93eec03a9a06805ea8400d1087
jasmind <jasmin.daguda@wizard.ba>
2016-01-20 15:20:10 +0100

    In memory raw base64 printing tryout v2

--- lib/printnode/printjob.rb
+++ lib/printnode/printjob.rb
@@ -10,6 +10,7 @@ module PrintNode
     attr_accessor :content
     attr_accessor :source
     attr_accessor :in_memory
+    attr_accessor :use_base64
 
     # Maps the object into a hash ready for JSON Encoding.
     def to_hash
@@ -17,27 +18,32 @@ module PrintNode
       hash['printerId'] = @printer_id
       hash['title'] = @title
       hash['contentType'] = @content_type
-      if @content_type.include?('base64')
-        if @in_memory
-          hash ['content'] = Base64.encode64(@content)
+      if @use_base64
+        if @content_type.include?('base64')
+          if @in_memory
+            hash ['content'] = Base64.encode64(@content)
+          else
+            hash ['content'] = Base64.encode64(IO.read(@content))
+          end
         else
-          hash ['content'] = Base64.encode64(IO.read(@content))
+          hash ['content'] = @content
         end
-    else
+      else
         hash ['content'] = @content
-      end
+      end  
       hash['source'] = @source
       hash
     end
 
     # Initializes the object with the variables required.
-    def initialize(printer_id, title, content_type, content, source, in_memory)
+    def initialize(printer_id, title, content_type, content, source, in_memory, use_base64)
       @printer_id = printer_id
       @title = title
       @content_type = content_type
       @content = content
       @source = source
       @in_memory = in_memory
+      @use_base64 = use_base64
     end
   end
 end
